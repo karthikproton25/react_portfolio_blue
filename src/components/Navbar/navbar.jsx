fd6f66e9a85d1f087ac71875cd4c2035bddbd039
Karthik <karthik2502@protonmail.com>
2024-03-12 19:51:31 +0530

    Changing mobile responsive components

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -5,26 +5,39 @@ import {getimageURL} from "../../utils";
 export const navbar = () => {
   return (
     <nav className={styles.navbar}>
-      <a  className={styles.title} href="/">Portfolio</a>
-      
-      <div className={styles.menu}>
-      <img className = {menuBtn} src={getimageURL("nav/menuIcon.png")} alt="menu button" />
-        <ul className={styles.menu}>
-          <li className={styles.menuitems}>
-            <a href="#about">About</a>
-          </li>
-          <li>
-            <a href="#experience">Experience</a>
-          </li>
-          <li>
-            <a href="#projects">Projects</a>
-          </li>
-          <li>
-            <a href="#contact">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <a className={styles.title} href="/">
+      Portfolio
+    </a>
+    <div className={styles.menu}>
+      <img
+        className={styles.menuBtn}
+        src={
+          menuOpen
+            ? getImageUrl("nav/closeIcon.png")
+            : getImageUrl("nav/menuIcon.png")
+        }
+        alt="menu-button"
+        onClick={() => setMenuOpen(!menuOpen)}
+      />
+      <ul
+        className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+        onClick={() => setMenuOpen(false)}
+      >
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#projects">Projects</a>
+        </li>
+        <li>
+          <a href="#about">Contact</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
   );
 
 };

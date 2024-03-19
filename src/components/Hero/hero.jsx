5cad844adb3e9804bfbd6f5e4ea93607183fc496
Karthik <karthik2502@protonmail.com>
2024-03-20 03:58:07 +0530

    adding hero section css modules

---
+++ src/components/Hero/hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const hero = () => {
+  return <section>
+    <div>
+        <h1>Hi, I'm Ada</h1>
+        <p>I'm a fullstack developer with 1 year of experience in React</p>
+    <a href="mail to:mymail@gmail.com">Contact Me</a>
+    </div>
+    </section>;
+    
+  
+}

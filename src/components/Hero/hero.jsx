43489cd6ea5e44a9f4e22e1ac5881c409514f392
Karthik <karthik2502@protonmail.com>
2024-03-22 01:46:07 +0530

    Hero images added

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { getImageUrl } from "../../../utils";
 
 export const hero = () => {
   return <section>
@@ -7,6 +8,9 @@ export const hero = () => {
         <p>I'm a fullstack developer with 1 year of experience in React</p>
     <a href="mail to:mymail@gmail.com">Contact Me</a>
     </div>
+    <img src="{getImageUrl"{"hero/heroImagepng"} alt="Hero Image" />
+    <div className={StyleSheet.topBlur} />
+    <div className={StyleSheet.bottomBlur} />
     </section>;
     
   

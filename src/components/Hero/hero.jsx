ba2f226dab3f1a76efc298ec04de5319087a57cf
Karthik <karthik2502@protonmail.com>
2024-03-24 00:35:43 +0530

    jsx files edited

--- src/components/Hero/hero.jsx
+++ src/components/Hero/hero.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import { getImageUrl } from "../../../utils";
 
 export const hero = () => {
-  return <section>
-    <div>
-        <h1>Hi, I'm Ada</h1>
-        <p>I'm a fullstack developer with 1 year of experience in React</p>
-    <a href="mail to:mymail@gmail.com">Contact Me</a>
+  return 
+  <section className={Styles.container}>
+    <div className={Styles.content}>
+        <h1 className={Styles.title}>Hi, I'm Ada</h1>
+        <p className={Styles.description}>I'm a fullstack developer with 1 year of experience in React</p>
+    <a href="mail to:mymail@gmail.com" className={Styles.contactBtn}>Contact Me</a>
     </div>
-    <img src="{getImageUrl"{"hero/heroImagepng"} alt="Hero Image" />
+    <img src= {getImageUrl("hero/heroImage.png") } alt="Hero Image" className={Styles.heroImg}/>
     <div className={StyleSheet.topBlur} />
     <div className={StyleSheet.bottomBlur} />
     </section>;

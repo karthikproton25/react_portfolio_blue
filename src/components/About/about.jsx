721d2b711c79de39143e72a4280724dfdf697a30
Karthik <karthik2502@protonmail.com>
2024-04-06 08:34:15 +0530

    Udating about css modules

--- src/components/About/about.jsx
+++ src/components/About/about.jsx
@@ -4,3 +4,12 @@ import styles from "./about.modules.css";
 import { getImageURL } from "../../utils";
 
 
+export const About = () => {
+    return {
+        <section className={styles.container} id="about">
+        
+        
+        </section>
+        
+    }
+}

2f74333f61200d201db94bc6c0a1c49bc3e5b941
Karthik <karthik2502@protonmail.com>
2024-04-10 09:36:25 +0530

    adding new block files

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -4,24 +4,44 @@ import history from "../../data/history.json"
 import { getImageUrl } from "../../utils"
 
 export const experience = () => {
-  return ( 
-  <section className={styles.container} id="experience">
-    <h2> className={styles.title} Experience</h2>
-    <div {styles.content}>
+  return (
+    <section className={styles.container} id="experience">
+      <h2> className={styles.title} Experience</h2>
+      <div {styles.content}>
         <div className={styles.skills}>
-          {skills.map((skill, id) =>{
+          {skills.map((skill, id) => {
             return (
-            <div id={key} className={styles.skill}>
-              <div className={styles.skillImageContainer}>
-                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              <div id={key} className={styles.skill}>
+                <div className={styles.skillImageContainer}>
+                  <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+                </div>
+                <p>{skills.title}</p>
               </div>
-              <p>{skills.title}</p>
-            </div>
             );
-            })}
+          })}
+        </div>
+        <ul className={styles.history}>
+          {history.map((historyItem, id) => {
+            return (
+              <li key={id} className={styles.historyItem}>
+                <img src={getImageUrl(historyItem.imageSrc)}
+                  alt={`${historyItem.organisation} Logo`}
+                />
+                <div className={styles.historyItemDetails}>
+                  <h3>{`${historyItem.role}, ${historyItem.organisation}`}</h3>
+                  <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                  <ul>
+                    {historyItem.experiences.map((experience, id) => {
+                      return <li key={id}>{experience}</li>;
+                    })}
+                  </ul>
+                </div>
+              </li>
+            )
+          }
+          )}
+        </ul>
       </div>
-        <ul></ul>
-    </div>
-  </section>
+    </section>
   );
 }

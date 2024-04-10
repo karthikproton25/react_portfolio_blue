eee1d330c7f60774dc21ceb72f73d818bf74bbeb
Karthik <karthik2502@protonmail.com>
2024-04-10 09:10:34 +0530

    adding new block files

--- src/components/Experience/experience.jsx
+++ src/components/Experience/experience.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
+import skills from "../../data/skills.json"
+import history from "../../data/history.json"
+import { getImageUrl } from "../../utils"
 
 export const experience = () => {
-  return <section id="experience">
-    <h2>Experience</h2>
-    <div>
-        <div></div>
-        <ul>ß</ul>
+  return ( 
+  <section className={styles.container} id="experience">
+    <h2> className={styles.title} Experience</h2>
+    <div {styles.content}>
+        <div className={styles.skills}>
+          {skills.map((skill, id) =>{
+            return (
+            <div id={key} className={styles.skill}>
+              <div className={styles.skillImageContainer}>
+                <img src={getImageUrl(skill.imageSrc)} alt={skill.title} />
+              </div>
+              <p>{skills.title}</p>
+            </div>
+            );
+            })}
+      </div>
+        <ul></ul>
     </div>
   </section>
+  );
 }
